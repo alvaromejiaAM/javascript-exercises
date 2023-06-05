76d0b6dfc195acd7cd575102ee85b2608f8392c2
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-06-05 15:45:11 -0500

    Add function to run test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let total = 0;
+  if(isNaN(num1) 
+  || isNaN(num2) 
+  || num1 < 0 
+  || num2 < 0
+  || typeof(num1) === 'string'
+  || typeof(num2) === 'string'){
+    return 'ERROR';
+  }
+  else if(num1 > num2){
+   for(; num1 >= num2; num1--){ 
+    total += num1;
+   }
+  }
+  else if (num2 > num1){
+    for(; num1 <= num2; num2--){
+      total += num2;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
