6d1b39ae3785068e5649d7464db602e71fefd96a
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-06-05 16:04:53 -0500

    Add function to determine leapyears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if(year % 4 === 0 && year % 100 === 0){ //determines century years
+    if(year % 400 === 0){
+      return true; 
+    }
+    return false;
+  }
+  if(year % 400 === 0 || year % 4 === 0){ //determines non century years
+    return true;
+  }
+  else{
+    return false;
+  }
 };
 
 // Do not edit below this line
