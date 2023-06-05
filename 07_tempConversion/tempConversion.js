8c524e5c61a92733409821f9732d9f881ee82e6f
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-06-05 16:18:13 -0500

    Update both functions with appropriate conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahTemp) {
+  let celsTemp = (fahTemp - 32) * (5/9)
+  return Math.round(celsTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsTemp) {
+  let fahTemp = celsTemp * (9/5) + 32;
+  return Math.round(fahTemp * 10) / 10;
 };
 
 // Do not edit below this line
