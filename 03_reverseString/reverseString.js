31ffaaadd85feef3c1e1e4635c8dec98e79acd31
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-06-05 10:51:59 -0500

    Add function to test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let reverseStr = '';
+  for(i = (word.length - 1); i >= 0; i--){
+     reverseStr += word.charAt(i);
+  }
+  return reverseStr;
 };
 
 // Do not edit below this line
