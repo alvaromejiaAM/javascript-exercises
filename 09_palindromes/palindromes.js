56b9af5b092b2c83b46ff21d5554fad1ed57d602
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-06-26 11:49:46 -0500

    Add solution to problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+//remove special char, spaces, and char sensitivity
+ str = str.toLowerCase();
+ str = str.replace(/[^a-zA-Z ]/g, "");
+ str = str.replace(/\s/g, '');
+ //string to char in array
+ const arr1 = str.split('');
+ const arr2 = [...arr1]; //copy
+ arr2.reverse();
+for(let i = 0; i < arr1.length; i++){
+    if(!(arr1[i] === arr2[i])){ //compare array char
+      return false;
+    }
+}
+return true;
 };
 
 // Do not edit below this line
