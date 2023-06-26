ad6d7d8212ce5f568bac5180e6461a03550ad204
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-06-26 10:32:02 -0500

    Add solutions for problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	let total = num1 + num2;
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let total = num1 - num2;
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  for(let i = 0; i < arr.length; i++){
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for(let i = 0; i < arr.length; i++){
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let total = 1;
+  if(num1 === 0){
+    return 1;
+  }
+	for(i = 1; i <= num1; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
