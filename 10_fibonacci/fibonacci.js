485d22d5ad39df02d4b21baf34dd09df8780cc44
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-06-27 15:02:21 -0500

    Add solutions to the problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let lastNum = 0,
+      currentNum = 1,
+      nextNum = 0;   
+  if(num < 0) return 'OOPS';
+  if(num === 0) return 0;
+  for(let i = 0; i < num-1; i++){
+    nextNum = lastNum + currentNum;
+    lastNum = currentNum;
+    currentNum = nextNum;
+  }
+  return currentNum;
 };
 
 // Do not edit below this line
