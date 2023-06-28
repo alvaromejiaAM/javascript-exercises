38277d9021bb39654624167d23e4b063e6d4bc43
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-06-28 10:33:26 -0500

    Add solution to problem

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let bookTitle = books.map(book => book.title);
+  
+  return bookTitle;
 };
 
 // Do not edit below this line
