4d67a49dd5bfcd162b5a5f0daeea7355c9218bd5
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-06-28 15:48:45 -0500

    Add solution to problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let currentTime = new Date();
+  let year = currentTime.getFullYear();
+  let oldestAge = 0;
+  let index = 0;
+  
+  let age = people.map((person) =>{
+    if('yearOfDeath' in person){
+      return person.yearOfDeath - person.yearOfBirth;
+    }
+    else return year - person.yearOfBirth;
+  });
 
+  
+  for(let i = 0; i < age.length; i++){
+    if(age[i] > oldestAge){
+      oldestAge = age[i];
+      index = i;
+    }
+  }
+ 
+  return people[index];
 };
 
 // Do not edit below this line
